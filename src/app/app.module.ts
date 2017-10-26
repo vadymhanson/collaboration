cb19685d8125b9f4673d78036a951caac389480b
Vadym Zazhyrko <Vadym.Zazhyrko-ext@censhare.com>
2017-10-26 17:56:48 +0300

    question creation page added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BackofficeComponent } from './backoffice/backoffice.component';
+import { QuestionEditorComponent } from './backoffice/question-editor/question-editor.component';
+import { QuestionListComponent } from './backoffice/question-list/question-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BackofficeComponent,
+    QuestionEditorComponent,
+    QuestionListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/backoffice/question-editor/question-editor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-question-editor',
+  templateUrl: './question-editor.component.html',
+  styleUrls: ['./question-editor.component.css']
+})
+export class QuestionEditorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
